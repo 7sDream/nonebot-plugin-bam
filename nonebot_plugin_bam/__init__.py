79571da56dcf3cddb85fb438adc4a0baf72e5668
7sDream <i@7sdre.am>
2023-06-15 22:22:48 +0800

    chore: release 0.2.0a1

--- nonebot_plugin_bam/__init__.py
+++ nonebot_plugin_bam/__init__.py
@@ -1,13 +1,13 @@
 from .activity import cmd_bilibili_activity_info
+from .bilibili.api import init_client
 from .config import Config
 from .follow import cmd_follower_add, cmd_follower_list, cmd_follower_remove
 from .group import cmd_group_add, cmd_group_list, cmd_group_remove
-from .bilibili.api import init_client
 from .tasks.activity_monitor import task_check_new_activity
 from .tasks.live_monitor import task_check_all_live_status
 from .user import cmd_user_fetch
 
-__version__ = "0.2.0"
+__version__ = "0.2.0a1"
 
 from nonebot.plugin import PluginMetadata
 
